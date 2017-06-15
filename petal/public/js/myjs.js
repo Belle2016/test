7b7cc4daec444c20a21c3417f3f9146b1cce2c57
Belle2016 <31401356@stu.zucc.edu.cn>
2017-06-15 12:04:10 +0800

    重构了文件结构，完善了页面
    重构了文件结构，完善了页面

---
+++ petal/public/js/myjs.js
@@ -0,0 +1,46 @@
+/**
+ * Created by lenovo on 2017/6/11.
+ */
+$(function(){
+    var $searchBar = $('#search_bar'),
+    // $searchResult = $('#searchResult'),
+    $searchText = $('#search_text'),
+    $searchInput = $('#search_input'),
+    $searchClear = $('#search_clear'),
+    $searchCancel = $('#search_cancel');
+
+    function hideSearchResult(){
+//        $searchResult.hide();
+        $searchInput.val('');
+    }
+    function cancelSearch(){
+   //     hideSearchResult();
+        $searchBar.removeClass('weui-search-bar_focusing');
+        $searchText.show();
+    }
+
+    $searchText.on('click', function(){
+        $searchBar.addClass('weui-search-bar_focusing');
+        $searchInput.focus();
+    });
+    $searchInput
+        .on('blur', function () {
+        if(!this.value.length) cancelSearch();
+    })
+    .on('input', function(){
+        if(this.value.length) {
+    //        $searchResult.show();
+        } else {
+      //      $searchResult.hide();
+        }
+    })
+    ;
+    $searchClear.on('click', function(){
+     //   hideSearchResult();
+        $searchInput.focus();
+    });
+    $searchCancel.on('click', function(){
+      //  cancelSearch();
+        $searchInput.blur();
+    });
+});
